32f23b97bf80d8d9fa70af5c9cf96207d9788dc1
Manuel Limones <limonesu.me@gmail.com>
2016-03-16 00:28:43 -0600

    Add base addresses for i2c magnetometer

--- source/main.c
+++ source/main.c
@@ -2,10 +2,19 @@
 #include "i2c_same70.h"
 #include "mcp4726.h"
 
+
+#define HMC5883_ADDRESS (0x1E)
+#define HMS5833_REG_MODE (0x02)
+#define HMS5833_REG_DATA_X_HIGH (0x03)
+#define HMS5833_REG_DATA_X_LOW (0x04)
+#define HMS5833_REG_DATA_Z_HIGH (0x05)
+#define HMS5833_REG_DATA_Z_LOW (0x06)
+#define HMS5833_REG_DATA_Y_HIGH (0x07)
+#define HMS5833_REG_DATA_Y_LOW (0x08)
+
+
 int main(){
     watchdog_disable();
-    mcp4726_init(2048);
-    mcp4726_output(2048);
     while(1){
     }
 }
